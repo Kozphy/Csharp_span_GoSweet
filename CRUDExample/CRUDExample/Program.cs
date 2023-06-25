31ccab9062d3444023619bf89bb3f025d466908c
Zixsa <50504962+Kozphy@users.noreply.github.com>
2023-06-26 00:26:27 +0800

    up

---
+++ CRUDExample/CRUDExample/Program.cs
@@ -0,0 +1,37 @@
+using CRUDExample.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CRUDExample
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+            builder.Services.AddControllersWithViews();
+            builder.Services.AddDbContext<testContext>(
+                    options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Home/Error");
+            }
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=StudentsClass}/{action=Index}/{id?}");
+
+            app.Run();
+        }
+    }
+}
