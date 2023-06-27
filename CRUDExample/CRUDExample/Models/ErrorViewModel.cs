6b3dbd464aede812dd797c8caf0dc01e56796dd5
Zixsa <50504962+Kozphy@users.noreply.github.com>
2023-06-27 22:08:30 +0800

    up

--- CRUDExample/CRUDExample/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace CRUDExample.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
