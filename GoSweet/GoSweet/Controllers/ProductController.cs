a1cde5aa59bd275383c7e8cebc2139557aeb9925
Liao <tommy40114@gmail.com>
2023-07-06 16:18:20 +0800

    商品

---
+++ GoSweet/GoSweet/Controllers/ProductController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BigWork_Test.Controllers {
+    public class ProductController : Controller {
+        public IActionResult Index() {
+            return View();
+        }
+
+        public IActionResult Product() {
+            return View();
+        }
+    }
+}
