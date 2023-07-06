fdd27b29ea4f3157a1fee14e6a55d6c9b7fc738b
jy0505 <huangjy0505@gmail.com>
2023-07-06 16:14:38 +0800

    Add FirmPage

---
+++ GoSweet/GoSweet/Controllers/FirmPageController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Lab_0704_Firm.Controllers {
+    public class FirmPageController : Controller {
+        public IActionResult ProductSale()
+        {
+            return View();
+        }
+
+        public IActionResult FirmData()
+        {
+            return View();
+        }
+
+        public IActionResult ProductSearch()
+        {
+            return View();
+        }
+    }
+}
