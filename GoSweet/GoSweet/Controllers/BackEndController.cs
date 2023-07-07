1e76c72157ba99de5c29e00ffab17f95e64ed983
Ian <64462172+cooker5566@users.noreply.github.com>
2023-07-07 13:46:33 +0800

    Add files via upload

---
+++ GoSweet/GoSweet/Controllers/BackEndController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace backend.Controllers
+{
+    public class BackEndController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+        public IActionResult MainPage()
+        {
+            return View();
+        }
+        public IActionResult ItemPage()
+        {
+            return View();
+        }
+        public IActionResult Banned()
+        {
+            return View();
+        }
+        public IActionResult FirmPage()
+        {
+            return View();
+        }
+        public IActionResult CustomPage()
+        {
+            return View();
+        }
+    }
+}
