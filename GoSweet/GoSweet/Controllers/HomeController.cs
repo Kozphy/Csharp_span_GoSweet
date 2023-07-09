ca16a9672107740f854581e61a1f9684dab6eebd
Zixsa <50504962+Kozphy@users.noreply.github.com>
2023-07-09 23:24:35 +0800

    up

--- GoSweet/GoSweet/Controllers/HomeController.cs
+++ GoSweet/GoSweet/Controllers/HomeController.cs
@@ -7,10 +7,12 @@ namespace GoSweet.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly shopwebContext _shopwebContext;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, shopwebContext shopwebContext)
         {
             _logger = logger;
+            _shopwebContext = shopwebContext;
         }
 
         public IActionResult Index()
