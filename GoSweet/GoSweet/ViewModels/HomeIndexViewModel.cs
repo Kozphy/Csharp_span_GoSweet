9d69d1d872657d025de65ee139ffe561f80ee78a
Zixsa <50504962+Kozphy@users.noreply.github.com>
2023-07-12 07:56:10 +0800

    change ViewModel

--- GoSweet/GoSweet/ViewModels/HomeIndexViewModel.cs
+++ GoSweet/GoSweet/ViewModels/HomeIndexViewModel.cs
@@ -1,19 +1,22 @@
-﻿namespace GoSweet.ViewModels
+﻿using GoSweet.Models;
+
+namespace GoSweet.ViewModels
 {
     public class HomeIndexViewModel
     {
-        public class productRankData {
-            public string? ProductName { get; set; }
-            public string? ProductCategory { get;set;}
-            public int ProductPrice { get; set; }
-            public string? ProductDescription { get; set; }
-            public int ProductBuyNumber { get; set; }
-        }
+        //public class productRankData {
+        //    public string? ProductName { get; set; }
+        //    public string? ProductCategory { get;set;}
+        //    public int ProductPrice { get; set; }
+        //    public string? ProductDescription { get; set; }
+        //    public int ProductBuyNumber { get; set; }
+        //}
 
-        public class productGroupBuyData { 
-            public string? ProductName { get; set; }
-            public string? GroupBuyName { get; set; }
+        //public class productGroupBuyData { 
+        //    public string? ProductName { get; set; }
+        //    public string? GroupBuyName { get; set; }
+        //}
+        public Product_datatable product_Datatable { get; set; };
 
-        }
     }
 }
