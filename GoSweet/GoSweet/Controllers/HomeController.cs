42ad463240a772a98af82397fce78164a88e6d0c
Zixsa <50504962+Kozphy@users.noreply.github.com>
2023-07-13 16:37:52 +0800

    up

--- GoSweet/GoSweet/Controllers/HomeController.cs
+++ GoSweet/GoSweet/Controllers/HomeController.cs
@@ -18,30 +18,7 @@ namespace GoSweet.Controllers
             // Order_datatable, o_buynumber
             // Member_membertable, m_nowpeople, g_maxpeople, Group_datatable g_end
             // 
-            var productRankData = from product in _shopwebContext.Product_datatables
-                               join product_pic in _shopwebContext.Product_picturetables on product.p_number equals product_pic.p_number
-                               join order in _shopwebContext.Order_datatables on product.p_number equals order.p_number 
-                               select new
-                               {
-                                   ProductName = product.p_number,
-                                   ProductCategory = product.p_category,
-                                   ProductPrice = product.p_price,
-                                   ProductDescription = product.p_describe,
-                                   ProductBuyNumber = order.o_buynumber,
-                               };
-            ViewData["productRankData"] = productRankData.ToList();
-
-            var productGroupBuyData = from product in _shopwebContext.Product_datatables
-                                  join product_pic in _shopwebContext.Product_picturetables on product.p_number equals product_pic.p_number
-                                  join groupbuy in _shopwebContext.Group_datatables on product.p_number equals groupbuy.p_number
-                                  select new
-                                  {
-                                        ProductName = product.p_name,
-                                        GroupBuyName = 
-                                  };
-
-            ViewData["productGroupBuyData"] = productGroupBuyData;
-
+            
             //var order_data = from order in _shopwebContext.Order_datatables
             //                 join product in _shopwebContext.Product_datatables on order.p_number equals product.p_number
             //                 select new
