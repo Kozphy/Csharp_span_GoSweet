d43477de84f99033b730c2390d7d2afb88c1b853
Zixsa <50504962+Kozphy@users.noreply.github.com>
2023-07-22 11:30:59 +0800

    update database

---
+++ GoSweet/GoSweet/Models/ViewModels/HomeIndexViewModel.cs
@@ -0,0 +1,38 @@
+﻿using GoSweet.Models;
+
+namespace GoSweet.Models.ViewModels
+{
+    public class HomeIndexViewModel
+    {
+        public IEnumerable<CategoryViewModel>? categoryViewModel { get; set; }
+        public IEnumerable<ProductRankDataViewModel>? productRankDatas { get; set; }
+        public IEnumerable<ProductGroupBuyData>? productGroupBuyDatas { get; set; }
+    }
+
+    public class CategoryViewModel
+    {
+        public string? Category { get; set; }
+    }
+
+    public class ProductRankDataViewModel
+    {
+        public string? ProductName { get; set; }
+        public string? ProductCategory { get; set; }
+        public string? ProductPicture { get; set; }
+        public int ProductPrice { get; set; }
+        public string? ProductDescription { get; set; }
+        public int ProductTotalBuyNumber { get; set; }
+    }
+
+    public class ProductGroupBuyData
+    {
+        public string? ProductName { get; set; }
+        public string? ProductPicture { get; set; }
+        public string? ProductDescription { get; set; }
+        public int GroupMaxPeople { get; set; }
+        public int GroupNowPeople { get; set; }
+        public double GroupPeoplePercent { get; set; }
+        public DateTime GroupEndDate { get; set; }
+        public int GroupRemainDate { get; set; }
+    }
+}
