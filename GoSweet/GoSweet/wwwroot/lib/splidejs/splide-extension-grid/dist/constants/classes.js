39d3c1ac3dceb44cf780ea0c9a491a01afc8c2b7
HTing-Yu <yu.822511@gmail.com>
2023-07-24 14:23:44 +0800

    update

---
+++ GoSweet/GoSweet/wwwroot/lib/splidejs/splide-extension-grid/dist/constants/classes.js
@@ -0,0 +1,4 @@
+import { CLASS_SLIDE } from '@splidejs/splide';
+export const CLASS_SLIDE_ROW = `${CLASS_SLIDE}__row`;
+export const CLASS_SLIDE_COL = `${CLASS_SLIDE}--col`;
+//# sourceMappingURL=../../src/js/constants/classes.js.map
