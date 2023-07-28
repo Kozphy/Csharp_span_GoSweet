9ec5af54e066a41d6bfe32c38663acdc54308ac1
Zixsa <50504962+Kozphy@users.noreply.github.com>
2023-07-28 09:52:08 +0800

    add hashpassword create

---
+++ GoSweet/GoSweet/Controllers/feature/HashPassword.cs
@@ -0,0 +1,27 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace GoSweet.Controllers.feature
+{
+    public class HashPassword
+    {
+        private string salt = "abc";
+        public  string CreateSha256Password(string password) { 
+           using(SHA256 sha256 = SHA256.Create())
+            {
+                // create salt password
+                string saltPassword = salt + password;
+
+                // Compute hash
+                byte[] bytes = sha256.ComputeHash(Encoding.UTF8.GetBytes(saltPassword));
+
+                StringBuilder builder = new StringBuilder();
+
+                foreach (byte b in bytes) {
+                    builder.Append(b.ToString("x2"));
+                }
+                return builder.ToString();
+            } 
+        }
+    }
+}
