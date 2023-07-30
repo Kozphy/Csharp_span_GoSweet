594d500262ad9f21e096236bd57c49a622a1fc99
Zixsa <50504962+Kozphy@users.noreply.github.com>
2023-07-30 10:37:23 +0800

    reformat dropdownMessage.js

--- GoSweet/GoSweet/wwwroot/js/Layout/bellDropDown.js
+++ GoSweet/GoSweet/wwwroot/js/Layout/bellDropDown.js
@@ -1,15 +1,13 @@
-﻿let bellIconBtn = document.querySelector(".bellIcon");
+﻿// Layout 通知欄顯示與隱藏
+let bellIconBtn = document.querySelector(".bellIcon");
 let bellDropDownContent = document.querySelector(".bell-dropdown-menu");
 bellIconBtn.addEventListener("click", function (e) {
     bellIconBtn.classList.toggle("active");
     bellDropDownContent.classList.toggle("bell-dropdown-menu-show");
 });
 
-//let bellRedCount = bellIconBtn.nextElementSibling;
-//let bellContent = document.querySelectorAll(".bellContent");
-//console.log(bellContent.length);
 
-
+// Layout 通知欄清除資訊
 let messageHaveReadBtn = document.querySelector(".messageHaveReadBtn");
 let firmMessageHaveReadBtn = document.querySelector(".firmMessageHaveReadBtn");
 
@@ -33,15 +31,22 @@ function clearNotifyMessage(btn, controllerName) {
 
 
         if (notifyMessageArray.length === 0) {
+            // 通知欄訊息清除 
+            for (let i = bellDropDownContent.childElementCount - 1; i >= 0; i--) {
+                bellDropDownContent.children[i].remove();
+            }
             renderDropDownBellMessageToNull(btn, spinnerBtn);
         }
     });
 }
 
+// Layout 通知欄渲染
 function renderDropDownBellMessageToNull(btn, spinnerBtn) {
 
     btn.classList.remove("d-none");
     spinnerBtn.classList.add("d-none");
+
+
     let htmlStr = `<a href="#" class="btn btn-hover-light rounded-0" style="width:300px">
                                             <div class="row align-items-center gy-2 gx-0">
                                                 <div class="col-3 text-center">
@@ -58,10 +63,6 @@ function renderDropDownBellMessageToNull(btn, spinnerBtn) {
                                         </button>
                                         `;
 
-    // remove all child element
-    for (let i = bellDropDownContent.childElementCount - 1; i >= 0; i--) {
-        bellDropDownContent.children[i].remove();
-    }
 
     bellDropDownContent.insertAdjacentHTML("beforeend", htmlStr);
     document.querySelector(".notify-message-counter").textContent = 0;
