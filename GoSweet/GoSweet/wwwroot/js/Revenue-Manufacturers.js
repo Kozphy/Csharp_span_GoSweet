0c8441c3c1506a17e1e3297fe2344a128b7b8288
HT-Yu <tim01125@hotmail.com>
2023-07-31 22:05:12 +0800

    表單名稱修正 排序觸發優化 圖片路徑更新

--- GoSweet/GoSweet/wwwroot/js/Revenue-Manufacturers.js
+++ GoSweet/GoSweet/wwwroot/js/Revenue-Manufacturers.js
@@ -16,7 +16,7 @@ window.onload =function () {
     var QuentityofXaxis = [];
     var color = ['rgba(195, 166, 160, 0.3)', 'rgba(161, 92, 56, 0.3)', 'rgba(255, 99, 132, 0.3)', 'rgba(255, 159, 64, 0.3)', 'rgba(255, 205, 86, 0.3)', 'rgba(75, 192, 192, 0.3)', 'rgba(54, 162, 235, 0.3)', 'rgba(153, 102, 255, 0.3)', 'rgba(201, 203, 207, 0.3)'];
     var ReportChart;
-    var ascendingOrder = true;
+    var ascendingOrder = false;
     var AscendingOrder = true;
     //Json資料參數
     var JsonData;
