18662cbc44d044e066161a56dcd50e7b3cf5facd
Zixsa <50504962+Kozphy@users.noreply.github.com>
2024-03-07 19:34:59 +0800

    add deleteAllData.sql sql file

--- webshop/All_In_one/DeleteAllData.sql
+++ webshop/All_In_one/DeleteAllData.sql
@@ -14,6 +14,7 @@ GO
 -- TRUNCATE all table
 EXEC sp_MSForEachTable "TRUNCATE TABLE ?"
 GO
+
 -- get table seed
 SELECT IDENT_CURRENT('TableName') AS SeedValue;
 EXEC sp_MSforEachtable "IDENT_CURRENT(?) AS SeedValue"
