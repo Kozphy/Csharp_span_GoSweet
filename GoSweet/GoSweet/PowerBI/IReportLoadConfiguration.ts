0cef067d042d682fa056d1d0e3ca839b96239b67
Zixsa <50504962+Kozphy@users.noreply.github.com>
2024-03-13 12:24:18 +0800

    add typescript and powerBI interface

---
+++ GoSweet/GoSweet/PowerBI/IReportLoadConfiguration.ts
@@ -0,0 +1,17 @@
+﻿interface IReportLoadConfiguration {
+    embedUrl: string;
+    accessToken: string;
+    id: string;
+    groupId?: string;
+    settings?: ISettings;
+    bookmark?: IApplyBookmarkRequest;
+    pageName?: string;
+    filters?: ReportLevelFilters[];
+    slicers?: ISlicer[];
+    theme?: IReportTheme;
+    contrastMode?: ContrastMode;
+    datasetBinding?: IDatasetBinding;
+    permissions?: Permissions;
+    viewMode?: ViewMode;
+    tokenType?: TokenType;
+}
